9024756f22494151d18009de393aae1495b3a90e
raulmarquez93 <raumarroi@alu.edu.gva.es>
2025-10-16 10:20:27 +0200

    fix: :bug: Error vite,config

--- proyecto/vite.config.js
+++ proyecto/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:'/portafolio-violeta/'
+  base:'/portafolio-violeta/',
   plugins: [tailwindcss()],
 })
